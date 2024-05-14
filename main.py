61d91e8cee171cb7f84280f301180c96b7d446c3
Joe <42589294+0xOsprey@users.noreply.github.com>
2024-05-13 21:35:03 -0700

    Update main.py

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ def webhook():
     except Exception as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        ts(exc_type, fname, exc_tb.tb_lineno)
+        print(exc_type, fname, exc_tb.tb_lineno)
         return '', 404
         
     
